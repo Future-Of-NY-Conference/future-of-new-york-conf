9082aa76cc3b8ed6b2dba3bf2e83468c40c39001
Francois Brill <f@brill.co.za>
2024-06-05 10:28:39 -0700

    Landing Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,74 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Head from "next/head"
+import Image from "next/image"
+import { Inter } from "next/font/google"
+import { ArrowUpRightIcon, BellIcon, CalendarIcon, MapPinIcon } from "@heroicons/react/24/outline"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={`${inter.className}`}>
+      <Head>
+        <title>The Future Of New York - Conference</title>
+      </Head>
+      <div className="flex min-h-screen flex-col items-center justify-between  bg-hero-gradient relative px-5 pt-72 md:pt-[350px] pb-20 bg-cover">
+        <div className="max-w-4xl text-center z-10">
+          <p className="text-sm font-medium text-[#192c55] bg-[#123480]/15 rounded-full py-2 px-5 inline-block backdrop-blur-md">
+            Connecting Leaders, Visionaries, and Change-Makers
+          </p>
+          <h1 className="text-5xl md:text-7xl mt-4">The Future Of New York</h1>
+          <div className="mt-7 md:text-lg text-gray-800 space-y-5">
+            <p>
+              Join us for an exclusive gathering, where elected and appointed officials, influential
+              figures, and ambitious thinkers will come together to make a lasting impact. Our goal
+              is to drive concrete progress on 3-5 moonshot projects that have the potential to
+              dramatically reshape our city for the better.
+            </p>
+          </div>
+          <div className="mt-7 flex flex-col md:flex-row items-center md:space-x-10 space-y-4 md:space-y-0 justify-center">
+            <div className="flex items-center space-x-3">
+              <CalendarIcon className="w-6 h-6 text-[#1A439E]" />
+              <p>Fall 2024</p>
+            </div>
+            <div className="flex items-center space-x-3">
+              <MapPinIcon className="w-6 h-6 text-[#1A439E]" />
+              <p>Manhattan, NY</p>
+            </div>
+          </div>
+          <div className="mt-10 md:mt-7 flex flex-col md:flex-row items-center md:space-x-5 space-y-2 md:space-y-0 justify-center">
+            <p>Presented by</p>
+            <Image src="/images/logo-mny.png" width={60} height={60} alt="Maximum New York Logo" />
+            <a
+              href="https://www.maximumnewyork.com/"
+              target="_blank"
+              className="border-b border-gray-600/20 hover:border-[#1A439E]/50 hover:text-[#1A439E] transition-all ease-in-out duration-200 relative group"
+            >
+              Maximum New York
+              <ArrowUpRightIcon className="w-3 h-3 absolute top-1 -right-4 text-[#1b49ad] hidden group-hover:inline-block" />
+            </a>
+          </div>
+          <div className="mt-10 md:mt-7">
+            <h2 className="text-2xl">Sign up for more updates on the venue and schedule.</h2>
+            <a
+              href="#"
+              target="_blank"
+              className="flex md:inline-flex items-center justify-center rounded-lg bg-gradient-to-t from-[#123480] to-[#123480] text-white px-6 py-4 shadow-lg mt-6 text-lg hover:from-[#123480] hover:to-[#1b49ad] border border-solid border-[#1b49ad] hover:-translate-y-1  transition-all ease-in-out duration-200"
+            >
+              <BellIcon className="w-6 h-6 mr-2 text-[#96B5FB]" />
+              Receive Updates
+            </a>
+          </div>
+        </div>
+        <div className="absolute top-14 left-[calc(50%-90px)]">
+          <Image
+            src="/images/empire-state-building.png"
+            alt="Empire State Building"
+            width={190}
+            height={683}
+            className="mix-blend-multiply opacity-40 md:opacity-100"
+          />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  );
+  )
 }
