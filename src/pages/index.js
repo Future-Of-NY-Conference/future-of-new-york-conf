49c93cfbd25c292b0fdfdd58e63f55380898aaf0
Francois Brill <f@brill.co.za>
2024-06-06 12:53:54 -0700

    Update host info

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,12 +17,12 @@ export default function Home() {
           <h1 className="text-5xl md:text-7xl mt-4 md:pr-6">The Future Of New York</h1>
           <div className="mt-7 md:text-lg text-gray-800 space-y-5">
             <p>
-              An invite-only working conference for elected/appointed officials and ambitious civic thinkers. 
-              Attendees will make concrete progress on 3-5 moonshot projects, all of which have the potential to
-              dramatically reshape New York City for the better.
+              An invite-only working conference for elected/appointed officials and ambitious civic
+              thinkers. Attendees will make concrete progress on 3-5 moonshot projects, all of which
+              have the potential to dramatically reshape New York City for the better.
             </p>
           </div>
-          <div className="mt-7 flex flex-col md:flex-row items-center md:space-x-10 space-y-4 md:space-y-0 justify-center">
+          <div className="mt-5 flex flex-col md:flex-row items-center md:space-x-10 space-y-4 md:space-y-0 justify-center">
             <div className="flex items-center space-x-3 md:min-w-[150px] md:justify-end">
               <CalendarIcon className="w-6 h-6 text-[#1A439E]" />
               <p>Fall 2024</p>
@@ -32,19 +32,34 @@ export default function Home() {
               <p>Manhattan</p>
             </div>
           </div>
-          <div className="mt-10 md:mt-7 flex flex-col md:flex-row items-center md:space-x-5 space-y-2 md:space-y-0 justify-center">
-            <p className="min-w-[150px] md:text-right">Presented by</p>
-            <Image src="/images/logo-mny.png" width={60} height={60} alt="Maximum New York Logo" />
-            <a
-              href="https://www.maximumnewyork.com/"
-              target="_blank"
-              className="border-b border-gray-600/20 hover:border-[#1A439E]/50 hover:text-[#1A439E] transition-all ease-in-out duration-200 relative group md:min-w-[150px]"
-            >
-              Maximum New York
-              <ArrowUpRightIcon className="w-3 h-3 absolute top-1 -right-4 text-[#1b49ad] hidden group-hover:inline-block" />
-            </a>
-          </div>
           <div className="mt-10 md:mt-7">
+            <h2 className="text-2xl">Presented by</h2>
+            <div className="flex justify-center items-center md:space-x-8 flex-col md:flex-row ">
+              {hosts?.map((host) => (
+                <div
+                  className="flex flex-row items-center space-x-3 mt-3 md:w-auto w-56"
+                  key={host.name}
+                >
+                  <Image
+                    src={`/images/${host.avatar}`}
+                    width={48}
+                    height={48}
+                    alt={host.name}
+                    className="rounded-full"
+                  />
+                  <a
+                    href={host.href}
+                    target="_blank"
+                    className="border-b border-gray-600/10 hover:border-[#1A439E]/50 hover:text-[#1A439E] transition-all ease-in-out duration-200 relative group"
+                  >
+                    {host.name}
+                    <ArrowUpRightIcon className="w-3 h-3 absolute top-1 -right-4 text-[#1b49ad] hidden group-hover:inline-block" />
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="mt-10 md:mt-8">
             <h2 className="text-2xl">
               Subscribe for updates and a thorough post-conference writeup.
             </h2>
@@ -71,3 +86,21 @@ export default function Home() {
     </main>
   )
 }
+
+const hosts = [
+  {
+    name: "Daniel Golliher",
+    avatar: "avatar-daniel.png",
+    href: "https://x.com/danielgolliher",
+  },
+  {
+    name: "Maximum New York",
+    avatar: "logo-mny.png",
+    href: "https://www.maximumnewyork.com/",
+  },
+  {
+    name: "Ben Orenstein",
+    avatar: "avatar-ben.png",
+    href: "https://x.com/r00k",
+  },
+]
